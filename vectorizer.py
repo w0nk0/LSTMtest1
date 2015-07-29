970576704f574404eafc9ebdf4175a69900e491f
W0nk0 The Sane <github@wonko.oib.com>
2015-07-29 17:13:14 +0200

    Moved some functions from derived classes into base class

--- vectorizer.py
+++ vectorizer.py
@@ -6,6 +6,14 @@ class Vectorizer:
         self.dictionary=[]
         self._new_feed(fulltext,cutoff)
 
+    def _vectify(self,vec):
+        v=np.array(vec)
+        if len(v.shape)==2:
+            vec=vec[-1]
+        if len(v.shape)>2:
+            raise TypeError("from_vector_.. got a non- 1- or 2-dimensional vector :("+str(v))
+        return list(vec)
+
     def save(self,binary_file_name):
         from pickle import dump
         with open(binary_file_name,"wb") as outfile:
@@ -26,7 +34,7 @@ class Vectorizer:
         return False
 
     def detokenize(self, token):
-        return ""+token or None
+        return str(token or None)
 
     def tokenize(self,stream):
         tokens=[]
@@ -102,11 +110,13 @@ class Vectorizer:
         return val
 
     def from_vector(self,vec):
+        vec = self._vectify(vec)
         winner = max(vec)
         return self.detokenize(self.item(vec.index(winner)))
 
     def from_vector_rand(self,vec,randomization=0.5):
         from random import random
+        vec = self._vectify(vec)
         srt = [(v,x) for x,v in enumerate(vec)]
         srt.sort()
         srt.reverse()
@@ -118,6 +128,7 @@ class Vectorizer:
     def from_vector_sampled(self,vec,randomness=0.01):
         from random import random
         from bisect import bisect
+        vec = self._vectify(vec)
         weights = vec
         total = 0
         cum_weights = []
@@ -138,10 +149,10 @@ class Vectorizer:
 
 def testdata():
     return """topkek. Was ist an dem Artikel nicht bescheuert? . . . Was unterscheidet die bewaffneten Gruppen des Staates dann noch von den bewaffneten Gruppen der Kartelle? . Mord bleibt Mord phrasen bleiben phrasenDann lege den Unterschied doch mal dar.  . Und nun?Da hast du deinen unterschied. . meine Fresse. Den Tag muss ich mir im Kalender markieren.  Huh?Das wir mal einer Meinung sind.  Deine Flagge deutet auf eine Tendenz zum Anarchosyndikalismus hin.  Ich bin wertekonservativ mit einem Hang zum Anarchokapitalismus.  Es gibt also eine gewisse Schnittmenge an Meinungen :)In der Tat.  Besonders viertel, wo sie mit BW13-Tattoos rumlaufen, sollte man meiden.  Das sind fiese Gangster. An der Aussage kann man ja sehen, das du noch nie dort warst. . . . . . . Die Rede von Frau Merkel davor:
-https://www. youtube. com/watch?v=lYX37ZljEto
+https://www.youtube.com/watch?v=lYX37ZljEto
 
 Die Rede von Herrn Gabriel danach:
-https://www. youtube. com/watch?v=6E6-mV0jJAAdie Antwort vom Gabriel war schon nicht schlecht!Ist ja in Ordnung, wenn das der Snowdens Edward macht, aber wehe dem, der das in der Bundesrepublik versucht. . . . . . . http://i. imgur. com/YCzMI. gifFind ich gut.  Hoffentlich kommt der Netzpolitik-Hampel in den Knast. . . . . . . . Was haben die Deutschen eigentlich nicht erfunden?Ricola. ich mag seine passiv aggresive art nicht. . Immer diese Leute die nicht verstehen was ein x-post ist. Bild oder es ist nicht passiert!Immer diese Leute die nicht verstehen was ein x-post ist. Also ich dachte zumindest du solltest wissen was ein x-post ist. [Well then](http://www. allmystery. de/i/t330413_unexpected-monk-meme-generator-do-me-a-f. jpg?bc). . . . Nein, Skrotum ist der Hodensack. Immer diese Leute die nicht verstehen was ein x-post ist. . Ist das jetzt gut, weil die AfD endlich als rechts-konvervative Partei bekannt wird, oder schlecht, weil sie endlich als rechts-konvervative Partei bekannt wird ?. Hat sich aber offiziell gerne als "liberal" und Alternative zur FDP dargestellst . . .  damit isses wohl nun zu Ende.  . . . . . . . Die AfD ist die vorzeitige  Nachfolgerpartei der NPD. . . . . . . .  oder auch der politische Arm der Pegida. . . . Schill.  [Vielleicht brauchen wird bald ein neues Lied](https://www. youtube. com/watch?v=bAl5HbBJe98). [Der braune PEGIDA-Dreckmob hat schon eins](https://soundcloud. com/yellowumbrella2/no-pegida-yellow-umbrella-ronny-trettmann-tiny-dawson). Ich ich auche Kacke.  Ich fand die AfD schon in vielen Punkten in Ordnung aber jetzt driften sie mir zu weit Rechts.
+https://www.youtube.com/watch?v=6E6-mV0jJAAdie Antwort vom Gabriel war schon nicht schlecht!Ist ja in Ordnung, wenn das der Snowdens Edward macht, aber wehe dem, der das in der Bundesrepublik versucht. . . . . . . http://i.imgur.com/YCzMI.gif Find ich gut.  Hoffentlich kommt der Netzpolitik-Hampel in den Knast. . . . . . . . Was haben die Deutschen eigentlich nicht erfunden?Ricola. ich mag seine passiv aggresive art nicht. . Immer diese Leute die nicht verstehen was ein x-post ist. Bild oder es ist nicht passiert!Immer diese Leute die nicht verstehen was ein x-post ist. Also ich dachte zumindest du solltest wissen was ein x-post ist. [Well then](http://www. allmystery. de/i/t330413_unexpected-monk-meme-generator-do-me-a-f. jpg?bc). . . . Nein, Skrotum ist der Hodensack. Immer diese Leute die nicht verstehen was ein x-post ist. . Ist das jetzt gut, weil die AfD endlich als rechts-konvervative Partei bekannt wird, oder schlecht, weil sie endlich als rechts-konvervative Partei bekannt wird ?. Hat sich aber offiziell gerne als "liberal" und Alternative zur FDP dargestellst . . .  damit isses wohl nun zu Ende.  . . . . . . . Die AfD ist die vorzeitige  Nachfolgerpartei der NPD. . . . . . . .  oder auch der politische Arm der Pegida. . . . Schill.  [Vielleicht brauchen wird bald ein neues Lied](https://www. youtube. com/watch?v=bAl5HbBJe98). [Der braune PEGIDA-Dreckmob hat schon eins](https://soundcloud. com/yellowumbrella2/no-pegida-yellow-umbrella-ronny-trettmann-tiny-dawson). Ich ich auche Kacke.  Ich fand die AfD schon in vielen Punkten in Ordnung aber jetzt driften sie mir zu weit Rechts.
 
 Es fehlt einfach eine ordentliche rechts-liberale Partie in Deutschland. Es gab da ja mal die FDP, aber deren Rolle hat jetzt ja schon die SPD eingenommen. . Bernd Lucke for President!!Jetzt mach mer mal langsam. &gt;Die Halle tobt, als Petry zum Finale ansetzt: Die Links-rechts-Einordnung vergifte die Demokratie im Land.
 """
